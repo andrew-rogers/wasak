b92d113ec033eb1401afeff8e159dd6740fac77c
Andrew Rogers <andrew.rogerstech@gmail.com>
2022-02-11 23:03:51 +0000

    Add Alpine Pi to downloads.

--- downloads.sh
+++ downloads.sh
@@ -6,6 +6,7 @@ get_downloads() {
     [ ! -f "firmware-stable.zip" ] && wget "https://github.com/raspberrypi/firmware/archive/stable.zip" -O "firmware-stable.zip"
     [ ! -f "busybox-armv7l" ] && wget "https://www.busybox.net/downloads/binaries/1.21.1/busybox-armv7l"
     [ ! -f "gen_init_cpio.c" ] && wget "https://github.com/raspberrypi/linux/raw/rpi-5.4.y/usr/gen_init_cpio.c"
+    [ ! -f "alpine-rpi-3.15.0-aarch64.tar.gz" ] && wget "https://dl-cdn.alpinelinux.org/alpine/v3.15/releases/aarch64/alpine-rpi-3.15.0-aarch64.tar.gz"
 }
 
 ( mkdir -p "$DOWNLOADS" && cd "$DOWNLOADS" && get_downloads )
