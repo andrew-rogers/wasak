920c8d9a78bb31e1332f09a60448af2f96a428e1
Andrew Rogers <andrew.rogerstech@gmail.com>
2022-02-12 06:54:15 +0000

    Add ttyd download.

--- downloads.sh
+++ downloads.sh
@@ -7,6 +7,7 @@ get_downloads() {
     [ ! -f "busybox-armv7l" ] && wget "https://www.busybox.net/downloads/binaries/1.21.1/busybox-armv7l"
     [ ! -f "gen_init_cpio.c" ] && wget "https://github.com/raspberrypi/linux/raw/rpi-5.4.y/usr/gen_init_cpio.c"
     [ ! -f "alpine-rpi-3.15.0-aarch64.tar.gz" ] && wget "https://dl-cdn.alpinelinux.org/alpine/v3.15/releases/aarch64/alpine-rpi-3.15.0-aarch64.tar.gz"
+    [ ! -f "ttyd-1.6.3-r3.apk" ] && wget "http://dl-cdn.alpinelinux.org/alpine/edge/community/aarch64/ttyd-1.6.3-r3.apk"
 }
 
 ( mkdir -p "$DOWNLOADS" && cd "$DOWNLOADS" && get_downloads )
